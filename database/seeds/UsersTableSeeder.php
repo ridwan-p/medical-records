9fb751402765a1216ef2c82f5538fd7f999ff8d4
Ridwan Pamungkas <ridwan@jogjacamp.co.id>
2019-12-05 09:18:09 +0700

    add seeder users

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        $user = factory(User::class)->create(['email' => 'default@mail.com']);
+    }
+}
