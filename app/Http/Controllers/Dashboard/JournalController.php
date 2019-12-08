0f4c96399a7483f6ca6c92636eec747e35da884c
Ridwan Pamungkas <ridwan.pamungks@gmail.com>
2019-12-08 21:51:02 +0700

    edit and destroy jurnal

--- app/Http/Controllers/Dashboard/JournalController.php
+++ app/Http/Controllers/Dashboard/JournalController.php
@@ -38,5 +38,43 @@ class JournalController extends Controller
 
     		return $journal;
     	});
+
+    	session()->flash('success', 'Add');
+    	return redirect()->route('dashboard.journals.index'); 
+    }
+
+    public function edit(Journal $journal)
+    {
+    	return view('dashboard.journals.edit', compact('journal'))
+    }
+
+    public function update(Request $request, Journal $journal)
+    {
+    	$request->validate([
+    		'patient_id' => 'required|exists:patients,id',
+    		'therapy' => 'required',
+			'note' => 'nullable',
+			'anamnese' => 'required',
+			'diagnosis' => 'required',
+    	]);
+
+    	$journal = DB::transaction(function () use ($request, $journal) {
+    		$journal->fill($request->all());
+    		$journal->save();
+
+    		return $journal;
+    	});
+
+    	session()->flash('success', 'Add');
+    	return redirect()->route('dashboard.journals.index'); 
+    }
+
+    public function destroy(Journal $journal)
+    {
+    	$journal->medications()->detach();
+    	$journal->delete();
+
+    	session()->flash('success', 'delete');
+    	return back();
     }
 }
