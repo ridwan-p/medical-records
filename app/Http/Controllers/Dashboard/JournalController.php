89afd6a8e9444e4e54a185c823454c63d9405c33
Ridwan Pamungkas <ridwan.pamungks@gmail.com>
2019-12-08 21:13:01 +0700

    view journals

--- app/Http/Controllers/Dashboard/JournalController.php
+++ app/Http/Controllers/Dashboard/JournalController.php
@@ -3,17 +3,40 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Journal;
+use App\Patient;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class JournalController extends Controller
 {
     public function index(Request $request)
     {
-    	# code...
+    	return view('dashboard.journals.index');
     }
 
     public function create()
     {
-    	# code...
+    	$patients = Patient::all();
+
+    	return view('dashboard.journals.create', compact('patients'));
+    }
+
+    public function store(Request $request)
+    {
+    	$request->validate([
+    		'patient_id' => 'required|exists:patients,id',
+    		'therapy' => 'required',
+			'note' => 'nullable',
+			'anamnese' => 'required',
+			'diagnosis' => 'required',
+    	]);
+
+    	$journal = DB::transaction(function () use ($request) {
+    		$journal = new Journal($request->all());
+    		$journal->save();
+
+    		return $journal;
+    	});
     }
 }
