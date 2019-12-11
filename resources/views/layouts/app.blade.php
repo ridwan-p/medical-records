58e0f97be821374ff264a5eca6509fecd35b3b1a
Ridwan Pamungkas <ridwan.pamungks@gmail.com>
2019-12-11 23:28:11 +0700

    lang in head title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ __(config('app.name', 'Laravel')) }}</title>
 
     <!-- Scripts -->
     <script src="{{ mix('js/manifest.js') }}" defer></script>
