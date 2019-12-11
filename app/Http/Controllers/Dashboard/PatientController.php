b32c2e0b2073a634594b35dc309e923b307167e8
Ridwan Pamungkas <ridwan.pamungks@gmail.com>
2019-12-11 23:30:24 +0700

    request perpage in patient

--- app/Http/Controllers/Dashboard/PatientController.php
+++ app/Http/Controllers/Dashboard/PatientController.php
@@ -19,7 +19,7 @@ class PatientController extends Controller
             }
         })
         ->orderBy($request->column ?? 'created_at', $request->direction ?? 'desc')
-        ->paginate(8);
+        ->paginate($request->per_page ?? 16);
 
     	return view('dashboard.patients.index', compact('patients'));
     }
