ca8888732c9bf3b3bdb84382ab98a96a90b64ad6
Ridwan Pamungkas <ridwan.pamungks@gmail.com>
2019-12-11 22:55:14 +0700

    indon lang

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
             <div class="container-fluid">
                 <a class="navbar-brand" href="{{ url('/') }}">
                      <img src="{{ asset('images/heart.svg') }}" width="30" height="30" alt="logo-brand">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ __(config('app.name', 'Laravel')) }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
