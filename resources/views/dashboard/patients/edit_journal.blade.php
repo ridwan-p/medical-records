8baf8928097ff3c5f8cda1acb8eef4cf821f0386
Ridwan Pamungkas <ridwan@jogjacamp.co.id>
2019-12-13 10:56:04 +0700

    message validation edit journal in user ;

--- resources/views/dashboard/patients/edit_journal.blade.php
+++ resources/views/dashboard/patients/edit_journal.blade.php
@@ -15,8 +15,8 @@
 		<div class="row">
 			<div class="form-group col-md-12">
 				<label for="therapy">{{ __('Therapy') }} <span class="text-danger">*</span></label>
-				<input type="text" class="form-control @error('therapy[]') is-invalid @enderror" id='therapy' name="therapy[]" value={{ old('therapy[]', $journal->therapy[0]) }}>
-				@error('therapy[]')
+				<input type="text" class="form-control @error('therapy.*') is-invalid @enderror" id='therapy' name="therapy[]" value={{ old('therapy[]', $journal->therapy[0]) }}>
+				@error('therapy.*')
 	                <span class="invalid-feedback" role="alert">
 	                    <strong>{{ $message }}</strong>
 	                </span>
@@ -25,8 +25,8 @@
 
 			<div class="form-group col-md-12">
 				<label for="anamnese">{{ __('Anamnese') }} <span class="text-danger">*</span></label>
-				<input type="text" class="form-control @error('anamnese[]') is-invalid @enderror" id='anamnese' name="anamnese[]" value={{ old('anamnese[]', $journal->anamnese[0]) }}>
-				@error('anamnese[]')
+				<input type="text" class="form-control @error('anamnese.*') is-invalid @enderror" id='anamnese' name="anamnese[]" value={{ old('anamnese[]', $journal->anamnese[0]) }}>
+				@error('anamnese.*')
 	                <span class="invalid-feedback" role="alert">
 	                    <strong>{{ $message }}</strong>
 	                </span>
@@ -35,8 +35,8 @@
 
 			<div class="form-group col-md-12">
 				<label for="diagnosis">{{ __('Diagnosis') }} <span class="text-danger">*</span></label>
-				<input type="text" class="form-control @error('diagnosis[]') is-invalid @enderror" id='diagnosis' name="diagnosis[]" value={{ old('diagnosis[]', $journal->diagnosis[0]) }}>
-				@error('diagnosis[]')
+				<input type="text" class="form-control @error('diagnosis.*') is-invalid @enderror" id='diagnosis' name="diagnosis[]" value={{ old('diagnosis[]', $journal->diagnosis[0]) }}>
+				@error('diagnosis.*')
 	                <span class="invalid-feedback" role="alert">
 	                    <strong>{{ $message }}</strong>
 	                </span>
@@ -45,8 +45,8 @@
 
 			<div class="form-group col-md-12">
 				<label for="medications">{{ __('Medications') }} <span class="text-danger">*</span></label>
-				<input type="text" class="form-control @error('medications[]') is-invalid @enderror" id='medications' name="medications[][name]" value={{ old('medications[][name]', $journal->medications[0]->name) }}>
-				@error('medications[][name]')
+				<input type="text" class="form-control @error('medications.*.name') is-invalid @enderror" id='medications' name="medications[][name]" value={{ old('medications[][name]', $journal->medications[0]->name) }}>
+				@error('medications.*.name')
 	                <span class="invalid-feedback" role="alert">
 	                    <strong>{{ $message }}</strong>
 	                </span>
