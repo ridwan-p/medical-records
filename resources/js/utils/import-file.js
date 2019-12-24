5ac4e7b79cd47fcb9e8283673d94a5d4bc06bbcd
Ridwan Pamungkas <ridwan.pamungks@gmail.com>
2019-12-24 17:18:02 +0700

    change custom-input-file label

--- resources/js/utils/import-file.js
+++ resources/js/utils/import-file.js
@@ -9,4 +9,10 @@ document.addEventListener('change', function(event) {
   if (event.target.matches('.import-file-upload')) {
     event.target.closest('form').submit()
   }
+
+  if(event.target.matches('.custom-file-input')) {
+    const fileName = event.target.files[0].name;
+    let nextSibling = event.target.nextElementSibling
+    nextSibling.innerText = fileName
+  }
 })
