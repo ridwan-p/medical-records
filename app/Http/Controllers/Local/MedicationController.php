9917b6456d74e494c985ae33457fb0391b30d804
Ridwan Pamungkas <ridwan.pamungks@gmail.com>
2020-01-21 13:18:30 +0700

    show all medicaiton

--- app/Http/Controllers/Local/MedicationController.php
+++ app/Http/Controllers/Local/MedicationController.php
@@ -10,12 +10,25 @@ class MedicationController extends Controller
 {
     public function index(Request $request)
     {
-    	$medications = Medication::when(@$request->q, function($query, $keyword) {
-	    		$query->where('name', 'like', "%{$keyword}%");
-	    	})
-	    	->orderBy($request->column ?? "created_at", $request->direction ?? 'asc')
+    	$medications = $this->getMedication($request)
 	    	->paginate($request->per_page);
 
     	return response()->json($medications);
+    }
+
+    public function showAll(Request $request)
+    {
+    	$medications = $this->getMedication($request)
+	    	->get();
+
+    	return response()->json($medications);
+    }
+
+    private function getMedication(Request $request)
+    {
+    	return Medication::when(@$request->q, function($query, $keyword) {
+	    		$query->where('name', 'like', "%{$keyword}%");
+	    	})
+	    	->orderBy($request->column ?? "created_at", $request->direction ?? 'asc');
     }
 }
