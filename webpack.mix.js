6258a88e8646325ca62c3023501c6773235ba1f6
Ridwan Pamungkas <ridwan.pamungks@gmail.com>
2020-01-21 15:45:38 +0700

    production

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
-   .extract(['vue', 'bootstrap', 'jquery']);
+   .extract([
+		'vue',
+		'bootstrap',
+		'jquery',
+		'apexcharts',
+		'vue-apexcharts'
+	]);
